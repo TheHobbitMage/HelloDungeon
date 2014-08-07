1a69bef67638a8c242be43d4d13d3971f9917876
Andrew Park <apark18@users.noreply.github.com>
2014-08-08 08:33:04 +0900

    Update HelloDungeon2.java

--- HelloDungeon2/HelloDungeon2.java
+++ HelloDungeon2/HelloDungeon2.java
@@ -3,7 +3,9 @@ public class HelloDungeon2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("HelloDungeon2");
-		
+	System.out.println("By Andrew Park");
+	Thread.sleep(1000);//not sure if this works but we will see
+	System.out.println("/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n")
 		
 		
 		
