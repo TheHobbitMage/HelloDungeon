8514f7e352f606a95ca208eadac9257cea85c400
Andrew Park <apark18@users.noreply.github.com>
2014-08-08 21:35:35 +0900

    Update HelloDungeon2.java

--- HelloDungeon2/HelloDungeon2.java
+++ HelloDungeon2/HelloDungeon2.java
@@ -1,11 +1,14 @@
-import java.util.*
+import java.util.*;
 public class HelloDungeon2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("HelloDungeon2");
 	System.out.println("By Andrew Park");
-	Thread.sleep(1000);//not sure if this works but we will see
-	System.out.println("/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n")
+	try {
+		  Thread.sleep(1000L);    // one second
+		}
+		catch (Exception e) {}
+	System.out.println("\n\n\n\n\n\n\n\ntest");
 		
 		
 		
