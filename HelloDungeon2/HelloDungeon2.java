ad0f279167ffc1dfa30c52959f3b560e89e40dda
Andrew Park <apark18@users.noreply.github.com>
2014-08-11 15:01:11 +0900

    Update HelloDungeon2.java

--- HelloDungeon2/HelloDungeon2.java
+++ HelloDungeon2/HelloDungeon2.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class HelloDungeon2 {
 	public static void main(String[] args) {
+float loop = 0;
 		// TODO Auto-generated method stub
 	System.out.println("HelloDungeon2");
 	System.out.println("By Andrew Park");
@@ -8,16 +9,27 @@ public class HelloDungeon2 {
 		  Thread.sleep(1000L);    // one second
 		}
 		catch (Exception e) {}
-	System.out.println("\n\n\n\n\n\n\n\ntest");
-		
-		
-		
+System.out.println("The Dragon Slayer has been long gone\nA new hero has been born");
+try {
+		  Thread.sleep(1000L);    // one second
+		}
+		catch (Exception e) {}
+while(loop == 0){//flag2
+Scanner name = new Scanner(System.in);
+	System.out.println("\n\n\n\n\n\n\n\nWhat is your name?");
+String hero = name.nextLine();
+		while(fighter == 0){//flag1
+		System.out.println("Okay, " + hero + ", what class do you pick?");
+
+
+
+		}//flag1
 		
 		
 		
 		
 	
 	
-	
+	}//flag2
 	}
 }
