1438284c053b1b4d782549f9a8991795f78d50cc
Andrew Park <apark18@users.noreply.github.com>
2014-08-23 17:26:46 -0400

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,9 +1,9 @@
 cd "$(dirname "$0")"
-echo HelloDungeon Starting Client
-echo Launch which game?
-echo 1) HelloDungeon
-echo 2) HelloDungeon2
-echo -n Option: 
+echo "HelloDungeon Starting Client"
+echo "Launch which game?"
+echo "1) HelloDungeon"
+echo "2) HelloDungeon2"
+echo -n "Option: "
 read option
 if [ $option = 1 ]
 then
