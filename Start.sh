38ff1d488b010867c3b34caa14ed9843bac95359
Andrew Park <apark18@users.noreply.github.com>
2014-08-23 17:02:04 -0400

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,1 +1,14 @@
-Echo Start
+cd "$(dirname "$0")"
+Echo HelloDungeon Starting Client
+Echo Launch which game?
+Echo 1) HelloDungeon
+Echo 2) HelloDungeon2
+Echo -n Option: 
+read option
+if [ $option = 1 ]
+then
+	//run code
+elif [ $option = 2 ]
+then
+	//run code
+fi
